e57b01211c9dfa0788beb717d75cd328560e60fa
Philippe <ducassephi@hotmail.fr>
2023-12-14 12:04:02 +0100

    fixed prod settings

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
