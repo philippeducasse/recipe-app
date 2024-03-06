0cadcd1546bca13e0722fa4fc29fd0e8678b7a13
philo <ducassephi@hotmail.fr>
2024-03-06 12:02:16 +0100

    upgraded db credentials and added pw to env var

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reci_please_db',
+        'USER': 'reci_please_db_user',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'dpg-cnk4d6i1hbls739remug-a',
+        'PORT': '5432',
     }
 }
 
