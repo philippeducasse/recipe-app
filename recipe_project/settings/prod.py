79b0be0fb86ffdcf5ba1d7118620fa8ebb1f9ef6
philo <ducassephi@hotmail.fr>
2024-03-06 12:14:36 +0100

    removed heroku configs

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -147,6 +147,6 @@ LOGIN_URL='/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
